8a37e1f8436332db6d0db80cb8b6aae72ea41899
Tim Zlomke <zlomke76@gmail.com>
2026-08-26 21:34:50 -0500

    Delete governance-adapter.ts

--- governance-adapter.ts
+++
@@ -1,3 +0,0 @@
-// Compatibility shim for an accidentally committed root-level adapter.
-// The canonical implementation lives in ./lib/governance-adapter.ts.
-export * from "./lib/governance-adapter";
